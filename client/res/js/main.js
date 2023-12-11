ce2f06666c930731a91bb2617278b1055f67c5b2
McBeefyVEVO <ondrastepo@gmail.com>
2023-12-11 01:04:03 -1000

    yeahbaby

---
+++ client/res/js/main.js
@@ -0,0 +1,39 @@
+const socket = io("http://localhost:3000");
+const info = document.getElementById("info");
+const socketId = document.getElementById("socketId");
+const chatInput = document.getElementById("chatInput");
+const chatSendButton = document.getElementById("chatSendButton");
+const chat = document.getElementById("chat");
+
+socket.on("connect", () => {
+    console.log(socket.id);
+    socketId.innerHTML = `Your id: ${socket.id}`
+    setInfo("You are connected! :3")
+});
+
+socket.on("message", (data) => {
+    chat.innerHTML += `<p>${data.id}: ${data.payload}</p>`
+});
+
+const sendData = (e) => {
+    e.preventDefault();
+    const data = {
+        id: socket.id,
+        payload: chatInput.value
+    };
+    socket.emit("message", data);
+}
+//chatSendButton.onclick = sendData;
+chatSendButton.addEventListener("click", sendData)
+
+
+let infoTimeout;
+
+const setInfo = (msg) => {
+    info.innerText = `Info: ${msg}`;
+    clearTimeout(infoTimeout);
+    infoTimeout = setTimeout(() => {
+        info.innerText = `Info: `;
+    }, 5000);
+}
+
